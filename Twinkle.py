4c1feefc59a6adb5b8358c233084a0a4b6a17e14
Twinkle <twinkle.r@somaiya.edu>
2024-09-24 18:45:25 -0400

    cool comment

---
+++ Twinkle.py
@@ -0,0 +1,35 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "559ca6ce-f3b8-4666-a2ef-37bf6286f3ca",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# I will write a comment here"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
